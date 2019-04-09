349f1912688aecc057649109d9010d74823c7c6b
ljy <874212844@qq.com>
2019-04-09 21:27:14 +0800

    新手教程-卜若

---
+++ Assets/Scripts/Bag/Bag.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Bag : MonoBehaviour {
+
+
+
+    /// <summary>
+    /// 生成背包框
+    /// </summary>
+    /// <param name="i">第几行</param>
+    /// <param name="j">第几列</param>
+    public void insBagItem(int i,int j)
+    {
+
+
+       GameObject g =  GameObject.Instantiate(ResourcesManager.prefabDic[ResName.bagItem],this.transform);
+       Image img = g.GetComponent<Image>();
+       img.sprite = new Sprite();
+
+    }
+
+
+    void Start()
+    {
+        insBagItem(1, 1);
+        
+    }
+
+}
